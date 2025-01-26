251640c832eeec17635764289d43b51cd16bd7b9
d-zhuye <180813277+d-zhuye@users.noreply.github.com>
2025-01-25 23:40:05 -0800

    Add & Test receiveAttack method
    Conditional statements to check if a ship is in cell; this will determine
    changes in the hit property of the cell.

--- src/modules/game-board.js
+++ src/modules/game-board.js
@@ -12,10 +12,10 @@ class GameBoard {
 
   createBoard() {
     let board = [];
-    const cell = { value: 0, shipId: null, hit: false };
     for (let row = 0; row < 10; row++) {
       let row = [];
       for (let col = 0; col < 10; col++) {
+        const cell = { value: 0, ship: null, hit: false };
         row.push(cell);
       }
       board.push(row);
@@ -24,15 +24,21 @@ class GameBoard {
   }
 
   receiveAttack(row, col) {
-    this.board[row][col].hit = "miss";
+    if (this.board[row][col].ship) {
+      this.board[row][col].hit = "hit";
+      this.board[row][col].ship.hit();
+    } else {
+      this.board[row][col].hit = "miss";
+    }
   }
 
   placeShip(ship, row, col) {
     ship.setCoords(row, col);
-    ship.rowPos.forEach(row => {
-      ship.colPos.forEach(col => {
-        this.board[row][col].value = 1;
-        this.board[row][col].shipId = ship.shipId;
+
+    ship.rowPos.forEach(a => {
+      ship.colPos.forEach(b => {
+        this.board[a][b].value = 1;
+        this.board[a][b].ship = ship;
       })
     })
   }
--- src/tests/game-board.test.js
+++ src/tests/game-board.test.js
@@ -1,3 +1,4 @@
+import { template } from "@babel/core";
 import { GameBoard } from "../modules/game-board";
 
 describe("create a 10x10 game board", () => {
@@ -11,7 +12,7 @@ describe("create a 10x10 game board", () => {
       expect(testBoard.board.length).toBe(10);
     })
     test("all 10 rows contains 10 arrays each with stored object", () => {
-      const expectedCell = {value: 0, shipId: null, hit: false};
+      const expectedCell = {value: 0, ship: null, hit: false};
       testBoard.board.forEach(row => {
         expect(row.length).toBe(10);
 
@@ -60,8 +61,8 @@ describe("create a 10x10 game board", () => {
 })
 
 describe("GameBoard object to properly manage ships", () => {
+  const testBoard2 = new GameBoard();
   describe("GameBoard to set coordinates of ships and update graph accordingly", () => {
-    const testBoard2 = new GameBoard();
     test("GameBoard to set coordinates of ships", () => {
       testBoard2.placeShip(testBoard2.submarine, 3, 7);
       expect(testBoard2.submarine.rowPos).toEqual([3]);
@@ -69,12 +70,31 @@ describe("GameBoard object to properly manage ships", () => {
     })
     test("placeShip method to throw error if ship is out of bound", () => {
       expect(() => testBoard2.placeShip(testBoard2.battleship, 8, 8)).toThrow("Ship out of bounds. Please reposition.");
+      expect(testBoard2.board[9][9].value).toBe(0);
+      expect(testBoard2.board[8][8].ship).toBe(null);
     })
     test("Setting ship coordinates should change corresponding cell values to 1  and shipId", () => {
-      const expected = { value: 1, shipId: "submarine", hit: false}
+      const expected = { value: 1, ship: testBoard2.submarine, hit: false}
       expect(testBoard2.board[3][7]).toEqual(expected);
       expect(testBoard2.board[3][8]).toEqual(expected);
       expect(testBoard2.board[3][9]).toEqual(expected);
+    })
+  })
+  describe("when cell with ship is hit, cell hit property and ship hit count  will change", () => {
+    const testBoard3 = new GameBoard();
+    testBoard3.placeShip(testBoard3.destroyer, 4, 4);
+    testBoard3.receiveAttack(4, 4);
+    test("cell hit property will change to hit", () => {
+      const expected = { value: 1, ship: testBoard3.destroyer, hit: "hit" }
+      expect(testBoard3.board[4][4]).toEqual(expected);
+    })
+    test("ship hit count increase by 1 when receiveAttack successfully hits", () => {
+      expect(testBoard3.destroyer.hitCount).toBe(1);
+    })
+    test("ship sunken status changed to true when hitCount equal length", () => {
+      testBoard3.receiveAttack(4, 5);
+      expect(testBoard3.destroyer.hitCount).toBe(2);
+      expect(testBoard3.destroyer.sunken).toBe(true);
     })
   })
 })
